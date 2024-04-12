e59b5da3fde74741e1778359e95466345a6e3e67
gmani1803 <166863103+gmani1803@users.noreply.github.com>
2024-04-12 19:53:20 +0100

    solution_exercise-1.sql

--- responses/exercise-1.sql
+++ responses/exercise-1.sql
@@ -1,2 +1,20 @@
 -- write your SQL here
-select 1;
+SELECT
+    EXTRACT(MONTH FROM TO_TIMESTAMP(t.datetime, 'YYYY-MM-DD HH24:MI:SS')) AS refund_month,
+    EXTRACT(YEAR FROM TO_TIMESTAMP(t.datetime, 'YYYY-MM-DD HH24:MI:SS')) AS refund_year,
+    oi.locationId,
+    SUM(CAST(json_extract_path_text(t.details, 'items', '0', 'amount') AS NUMERIC)) AS total_refund_amount
+FROM
+    transaction_data t
+JOIN
+    order_items oi ON CAST(json_extract_path_text(t.details, 'items', '0', 'id') AS INTEGER) = oi.id
+WHERE
+    t.type = 'refund'
+GROUP BY
+    refund_month,
+    refund_year,
+    oi.locationId
+ORDER BY
+    refund_year,
+    refund_month,
+    oi.locationId;
